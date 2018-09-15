53c5924b5b28a50bbeaf74b56bea77a8986ee291
stallman <gnuhub@icloud.com>
2018-09-15 13:52:04 +0800

    change to functions

--- osx.init.sh
+++ osx.init.sh
@@ -1,37 +1,56 @@
 #!/usr/bin/env bash
 set -x
-# https://developer.apple.com/download/more/
-if [ ! -f 'xcode9.4.1.pkg' ];then
-	wget https://download.sf.net/gnuhub/xcode9.4.1.pkg
-fi	
 
-if [ ! -f 'xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg' ];then
-	wget https://download.sf.net/gnuhub/xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg
-fi	
-# wget https://download.sf.net/gnuhub/xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg
-export __CFPREFERENCES_AVOID_DAEMON=1
+function pkgs.dowload()
+{
+    # https://developer.apple.com/download/more/
+    if [ ! -f 'xcode9.4.1.pkg' ];then
+	    wget https://download.sf.net/gnuhub/xcode9.4.1.pkg
+    fi
 
-sudo installer -pkg 'xcode9.4.1.pkg' -target /
+    if [ ! -f 'xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg' ];then
+	    wget https://download.sf.net/gnuhub/xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg
+    fi
+}
 
-curl curl -fsSL https://raw.githubusercontent.com/gnuhub/8_macos_init/master/agree_xcode_licence.sh > agree_xcode_licence.sh
-chmod +x agree_xcode_licence.sh
-./agree_xcode_licence.sh
+function xcode.install()
+{
+    # wget https://download.sf.net/gnuhub/xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg
+    export __CFPREFERENCES_AVOID_DAEMON=1
 
-sudo installer -pkg 'xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg' -target /
+    sudo installer -pkg 'xcode9.4.1.pkg' -target /
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    curl curl -fsSL https://raw.githubusercontent.com/gnuhub/8_macos_init/master/agree_xcode_licence.sh > agree_xcode_licence.sh
+    chmod +x agree_xcode_licence.sh
+    ./agree_xcode_licence.sh
 
-# https://mirror.tuna.tsinghua.edu.cn/help/homebrew/
-cd "$(brew --repo)"
-git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+    sudo installer -pkg 'xcode_cli_tools_macos10.13_for_xcode9.4.1.pkg' -target /
+}
 
-cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+function homebrew.install()
+{
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew update
+    # https://mirror.tuna.tsinghua.edu.cn/help/homebrew/
+    cd "$(brew --repo)"
+    git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
 
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
-export HOMEBREW_NO_AUTO_UPDATE=true
-brew install wget
-brew install gitlab-runner
-brew services start gitlab-runner
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+
+    brew update
+}
+
+function gitlab-runner.install()
+{
+    export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+    export HOMEBREW_NO_AUTO_UPDATE=true
+    brew install wget
+    brew install gitlab-runner
+    brew services start gitlab-runner
+}
+
+pkgs.dowload
+xcode.install
+homebrew.install
+gitlab-runner.install
